da82e0d2ad286969486f35187637f9689cd8183c
Safe_Mode <safe2mode@gmail.com>
2018-10-23 19:04:40 +0300

    Adds progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="container">
+    <counter :quantity="quantity"></counter>
   </div>
 </template>
 
 <script>
+  import Counter from './components/Counter'
+
   export default {
     name: 'app',
     data () {
       return {
-
+        quantity: {
+          min: 0,
+          max: 10,
+          current: 0
+        }
       }
+    },
+    components: {
+      Counter
     }
   }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="counter" id="counter">
+    <h2 class="counter__title">Quotes added</h2>
+    <div class="counter__progress progress">
+      <div class="progress-bar"
+          role="progressbar"
+          :style="{ width: barWidth}"
+          :aria-valuenow="quantity.current"
+          :aria-valuemin="quantity.min"
+          :aria-valuemax="quantity.max">{{ `${quantity.current} / ${quantity.max}` }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'counter',
+    data () {
+      return {
+        bar: {
+          minWidthFactor: 0.5
+        }
+      }
+    },
+    props: {
+      quantity: Object
+    },
+    computed: {
+      barWidth () {
+        return (this.quantity.current < this.bar.minWidthFactor) ? `${this.quantity.max * this.bar.minWidthFactor}%` : `${this.quantity.current * this.quantity.max}%`
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+  .counter__progress {
+    min-height: 20px;
+  }
+</style>
