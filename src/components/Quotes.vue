2437504148e0fde8f4902652ce4acc4d32818647
Safe_Mode <safe2mode@gmail.com>
2018-11-11 20:03:24 +0300

    Add transitions

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,15 +1,18 @@
 <template lang="html">
   <div class="quotes jumbotron"
       id="quotes">
-    <ul class="quotes__list d-flex flex-wrap">
+    <transition-group class="quotes__list d-flex flex-wrap"
+        tag="ul"
+        name="fade">
       <li class="quotes__item col-sm-6 col-md-4 col-lg-3"
-          v-for="quote, index in quotes">
+          v-for="quote, index in quotes"
+          :key="quote">
         <quote @click.native="removeQuote(index)"
             :key="index">
           {{ quote }}
         </quote>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -34,7 +37,7 @@
   }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
   .quotes__list {
     margin: 0;
     padding: 0;
@@ -44,5 +47,23 @@
   .quotes__item {
     margin-top: 5px;
     margin-bottom: 5px;
+  }
+
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.4s;
+  }
+
+  .fade-leave-active {
+    opacity: 0;
+    position: absolute;
+  }
+
+  .fade-move {
+    transition: all 0.4s;
   }
 </style>
